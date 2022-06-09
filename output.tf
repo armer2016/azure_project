f4086f18a8428701d617aebe812303c9f56bd955
Mira <99509104+armer2016@users.noreply.github.com>
2022-06-08 20:38:58 -0500

    Update output.tf
    add changes

--- output.tf
+++ output.tf
@@ -1,22 +1,22 @@
-# output "vnet_id" {
-#   value = azurerm_virtual_network.azure_project.id
-# }
+output "vnet_id" {
+  value = azurerm_virtual_network.azure_project.id
+}
 
-# output "subnet" {
-#   value = azurerm_virtual_network.azure_project.subnet
-# }
+output "subnet" {
+  value = azurerm_virtual_network.azure_project.subnet
+}
 
-# output "subnet_id" {
-#   value = azurerm_virtual_network.azure_project.subnet.*.id[1]
+output "subnet_id" {
+  value = azurerm_virtual_network.azure_project.subnet.*.id[1]
 
-# }
-# output "vnet_name" {
-#   value = azurerm_virtual_network.azure_project.name
-# }
-# output "resource_group_name" {
-#   value = azurerm_resource_group.azure_project.name
-# }
+}
+output "vnet_name" {
+  value = azurerm_virtual_network.azure_project.name
+}
+output "resource_group_name" {
+  value = azurerm_resource_group.azure_project.name
+}
 
-# output "resource_group_location" {
-#   value = azurerm_resource_group.azure_project.location
-# }  
+output "resource_group_location" {
+  value = azurerm_resource_group.azure_project.location
+}  
