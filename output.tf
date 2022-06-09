045ad598743dd8e90ab44fdd4c9642ad835b4f77
Mira <99509104+armer2016@users.noreply.github.com>
2022-06-08 19:49:10 -0500

    added changes

--- output.tf
+++ output.tf
@@ -1,23 +1,22 @@
 output "vnet_id" {
-    value = azurerm_virtual_network.terraform.id
+  value = azurerm_virtual_network.azure_project.id
 }
 
-
-output "subnet"{
-    value = azurerm_virtual_network.terraform.subnet
+output "subnet" {
+  value = azurerm_virtual_network.azure_project.subnet
 }
 
 output "subnet_id" {
-  value = "${azurerm_virtual_network.terraform.subnet.*.id[1]}"
+  value = azurerm_virtual_network.azure_project.subnet.*.id[1]
 
 }
-output "vnet_name"{
-    value = azurerm_virtual_network.terraform.name
+output "vnet_name" {
+  value = azurerm_virtual_network.azure_project.name
 }
-output resource_group_name {
-  value = azurerm_resource_group.terraform.name
+output "resource_group_name" {
+  value = azurerm_resource_group.azure_project.name
 }
 
-output resource_group_location {
-   value = azurerm_resource_group.terraform.location
-}
+output "resource_group_location" {
+  value = azurerm_resource_group.azure_project.location
+}  
