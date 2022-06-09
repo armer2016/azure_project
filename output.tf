3bd8236110ffc8eb3190dd2b28b8f90fa06d8c80
Mira <99509104+armer2016@users.noreply.github.com>
2022-06-08 19:03:17 -0500

    Create output.tf
    added output.tf

---
+++ output.tf
@@ -0,0 +1,18 @@
+output "subnet"{
+    value = azurerm_virtual_network.terraform.subnet
+}
+
+output "subnet_id" {
+  value = "${azurerm_virtual_network.terraform.subnet.*.id[1]}"
+
+}
+output "vnet_name"{
+    value = azurerm_virtual_network.terraform.name
+}
+output resource_group_name {
+  value = azurerm_resource_group.terraform.name
+}
+
+output resource_group_location {
+   value = azurerm_resource_group.terraform.location
+}
